fd7dddaae066ec39fb387f86c5c949c2260a2416
dzulkifli-mq <santriquarta@gmail.com>
2022-02-18 14:10:27 +0700

    home responsive half

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -22,16 +22,17 @@ import { Link } from 'react-scroll';
 export default function Home() {
     const list = [
         "Merencanakan kegiatan penyelesaian studi, perkembangan karier, serta kehidupan peserta didik di masa yang akan datang.",
-        "Mengetahui hambatan dan kesulitan yang dihadapi peserta didik dalam studi, penyesuaian dengan lingkungan pendidikan dalam masyarakat."
+        "Mengetahui hambatan dan kesulitan yang dihadapi peserta didik dalam studi, penyesuaian dengan lingkungan pendidikan dalam masyarakat.",
+        "Menyesuaikan diri dengan lingkungan pendidikan dan lingkungan masyarakat."
     ];
     const numbers = [1, 2, 3, 4, 5];
     const isiBox = list.map((isilist) => {
         return <ListItem>
-            <div className="flex my-3">
+            <div className="flex my-3 sm-max:my-1">
                 <div className="">
                     <ListIcon as={MdCheckCircle} color='green.500' />
                 </div>
-                <div className="text-center">{isilist}</div>
+                <div className="text-center sm-max:text-justify">{isilist}</div>
             </div>
         </ListItem>;
     });
@@ -60,46 +61,46 @@ export default function Home() {
     return (
         <div className="h-full scroll-smooth">
             {/* Home page */}
-            <section id="home" className="w-full lg:p-12 h-screen bg-gradient-to-r to-blue-700 from-blue-500">
+            <section id="home" className="w-full lg:p-12 p-4 h-screen bg-gradient-to-r to-blue-700 from-blue-500">
                 <div className="flex h-min justify-between">
-                    <img src={Logo} className=" w-1/12" />
+                    <img src={Logo} className="w-1/12 sm-max:w-4/12" />
                     <a href="/log">
-                        <button className="rounded-full px-7 h-min p-2 text-white shadow-slate-400 bg-hijau">
+                        <button className="rounded-full px-7 h-min p-2 text-white shadow-slate-400 bg-hijau sm-max:px-4 sm-max:text-sm sm-max:py-1">
                             <text className="">Login</text>
                         </button>
                     </a>
                 </div>
-                <div className="w-full flex justify-center py-5">
+                <div className="w-full flex justify-center py-5 sm-max:hidden">
                     <div className="w-1/2 flex justify-around shadow-md text-white p-3 bg-blues rounded-3xl">
                         <Link smooth={true} duration={1000} to="home"><div className="px-7 rounded-full py-1 hover:bg-hijau">Home</div></Link>
                         <Link smooth={true} duration={1000} to="bk"><div className="px-7 rounded-full py-1 hover:bg-hijau">Bimbingan Konseling</div></Link>
                         <Link smooth={true} duration={1000} to="contact"><div className="px-7 rounded-full py-1 hover:bg-hijau">Contact</div></Link>
                     </div>
                 </div>
-                <div className="w-full px-4 flex py-10 justify-between">
-                    <div className="text-white w-6/10 h-1/2">
-                        <p className="text-5xl py-10">Ruang BK</p>
-                        <p className="w-2/3 text-xl mb-4">Aplikasi Layanan Bimbingan dan Konseling. Mempermudah kerja Konselor/Guru Bimbingan dan Konseling dalam proses Management (Pengelolaan), Delivery (Penyampaian) and Connectivity (Konektivitas).</p>
+                <div className="w-full px-4 md:flex py-10 justify-center sm-max:py-2">
+                    <div className="text-white w-6/10 h-1/2 sm-max:w-full sm-max:py-4">
+                        <p className="text-5xl py-10 sm-max:text-2xl sm-max:py-2">Ruang BK</p>
+                        <p className="w-2/3 text-xl mb-4 sm-max:text-sm sm-max:w-full">Aplikasi Layanan Bimbingan dan Konseling. Mempermudah kerja Konselor/Guru Bimbingan dan Konseling dalam proses Management (Pengelolaan), Delivery (Penyampaian) and Connectivity (Konektivitas).</p>
                         <div className=" border-b-8 border-hijau w-1/2 rounded-md"></div>
                     </div>
                     {/* <div className="w-1/10"></div> */}
-                    <div className="w-3/10">
-                        <img src={Comment} alt="heart" className="w-full " />
+                    <div className="w-3/10 sm-max:w-full sm-max:pt-6">
+                        <img src={Comment} alt="heart" className="w-full sm-max:w-8/12 sm-max:float-right" />
                         {/* <p className="border-hijau border-b-8 rounded-md "></p> */}
                     </div>
                 </div>
             </section>
             {/* Bimbingan konseling */}
-            <section id="bk" className="bg-gray-200 py-20 px-10">
+            <section id="bk" className="bg-gray-200 py-20 px-10 sm-max:px-4">
                 <div className="w-full justify-center flex">
-                    <div className="">
-                        <p className="text-3xl text-blue-700 font-bahnschrift font-medium">Bimbingan Konseling</p>
+                    <div className="text-center">
+                        <p className="text-3xl sm-max:text-xl text-blue-700 font-bahnschrift font-medium">Bimbingan Konseling</p>
                         <div className="border-b-8 border-hijau rounded-md"></div>
                     </div>
                 </div>
-                <div className="flex h-max justify-center py-10 mx-10">
-                    <div className="w-1/3 mr-5 bg-white rounded-xl py-5 shadow-lg">
-                        <div className="flex justify-center">
+                <div className="md:flex h-max justify-center py-10 mx-10 sm-max:py-2 sm-max:mx-2">
+                    <div className="w-1/3 sm-max:w-full md:mr-5 bg-white rounded-xl py-5 sm-max:py-3 sm-max:my-3 shadow-lg">
+                        <div className="flex justify-center sm-max:hidden">
                             <Circle
                                 size='120px'
                                 bg='#2776ED'
@@ -108,16 +109,25 @@ export default function Home() {
                                 <img src={Hand} alt="" className="h-20" />
                             </Circle>
                         </div>
-                        <div className="border-b-8 border-hijau rounded-md my-5 mx-32" />
-                        <div className="font-bold text-center text-lg">
+                        <div className="flex justify-center md:hidden">
+                            <Circle
+                                size='60px'
+                                bg='#2776ED'
+                                color='white'
+                            >
+                                <img src={Hand} alt="" className="h-10" />
+                            </Circle>
+                        </div>
+                        <div className="border-b-8 border-hijau rounded-md my-5 mx-32 sm-max:my-2 sm-max:mx-20" />
+                        <div className="font-bold text-center text-lg sm-max:text-base">
                             Apa itu Bimbingan Konseling ?
                         </div>
-                        <div className="text-lg my-5 text-gray-400 text-center mx-10">
+                        <div className="text-lg my-5 sm-max:text-sm text-gray-400 text-center mx-10 sm-max:mx-3 sm-max:my-2">
                             serangkaian kegiatan berupa bantuan yang dilakukan seorang ahli pada konseling dengan cara tatap muka, baik secara individu atau beberapa orang dengan memberikan pengetahuan tambahan.
                         </div>
                     </div>
-                    <div className="w-1/3 mr-5 bg-white rounded-xl py-5 shadow-lg">
-                        <div className="flex justify-center">
+                    <div className="w-1/3 sm-max:w-full md:mr-5 bg-white rounded-xl py-5 sm-max:py-3 sm-max:my-3 shadow-lg">
+                        <div className="flex justify-center sm-max:hidden">
                             <Circle
                                 size='120px'
                                 bg='#2776ED'
@@ -126,16 +136,25 @@ export default function Home() {
                                 <img src={Business} alt="" className="h-20" />
                             </Circle>
                         </div>
-                        <div className="border-b-8 border-hijau rounded-md my-5 mx-32" />
-                        <div className="font-bold text-center text-lg">
+                        <div className="flex justify-center md:hidden">
+                            <Circle
+                                size='60px'
+                                bg='#2776ED'
+                                color='white'
+                            >
+                                <img src={Business} alt="" className="h-10" />
+                            </Circle>
+                        </div>
+                        <div className="border-b-8 border-hijau rounded-md my-5 mx-32 sm-max:my-2 sm-max:mx-20" />
+                        <div className="font-bold text-center text-lg sm-max:text-base">
                             Apa itu Konseling ?
                         </div>
-                        <div className="text-lg my-5 text-gray-400 text-center mx-10">
+                        <div className="text-lg my-5 sm-max:text-sm text-gray-400 text-center mx-10 sm-max:mx-3 sm-max:my-2">
                             proses pemberian bantuan yang dilakukan oleh seorang ahli (disebut konselor/pembimbing) kepada individu yang mengalami sesuatu masalah (disebut konseli) yang bermuara pada teratasinya masalah yang dihadapi klien.
                         </div>
                     </div>
-                    <div className="w-1/3 bg-white rounded-xl py-5 shadow-lg">
-                        <div className="flex justify-center">
+                    <div className="w-1/3 sm-max:w-full bg-white rounded-xl py-5 sm-max:py-3 sm-max:my-3 sm-max:mb-1 shadow-lg">
+                        <div className="flex justify-center sm-max:hidden">
                             <Circle
                                 size='120px'
                                 bg='#2776ED'
@@ -144,16 +163,25 @@ export default function Home() {
                                 <img src={Costumer} alt="" className="h-20" />
                             </Circle>
                         </div>
-                        <div className="border-b-8 border-hijau rounded-md my-5 mx-32" />
-                        <div className="font-bold text-center text-lg">
+                        <div className="flex justify-center md:hidden">
+                            <Circle
+                                size='60px'
+                                bg='#2776ED'
+                                color='white'
+                            >
+                                <img src={Costumer} alt="" className="h-10" />
+                            </Circle>
+                        </div>
+                        <div className="border-b-8 border-hijau rounded-md my-5 mx-32 sm-max:my-2 sm-max:mx-20" />
+                        <div className="font-bold text-center text-lg sm-max:text-base">
                             Bidang Layanan
                         </div>
-                        <div className="text-lg my-5 text-gray-400 text-center mx-10">
+                        <div className="text-lg my-5 sm-max:text-sm text-gray-400 text-center mx-10 sm-max:mx-3 sm-max:my-2">
                             Konseling bisa dilakukan dalam berbagai bidang kehidupan, seperti di masyarakat, di dunia industri, membantu korban bencana alam, maupun di lingkungan pendidikan.
                         </div>
                     </div>
                 </div>
-                <div className="mx-10">
+                <div className="mx-10 sm-max:mx-2">
                     <Box
                         p='10'
                         boxShadow='lg'
@@ -161,7 +189,7 @@ export default function Home() {
                         rounded='xl'
                         height='100%'
                     >
-                        <div className="h-full flex justify-center">
+                        <div className="h-full flex justify-center sm-max:hidden">
                             <Circle
                                 size='120px'
                                 bg='#2776ED'
@@ -170,19 +198,28 @@ export default function Home() {
                                 <img src={Lamp} alt="" className="h-20" />
                             </Circle>
                         </div>
-                        <div className="border-t-8 border-hijau rounded-md my-5 mx-96"></div>
-                        <div className="font-bold text-xl h-full text-center">
+                        <div className="flex justify-center md:hidden">
+                            <Circle
+                                size='60px'
+                                bg='#2776ED'
+                                color='white'
+                            >
+                                <img src={Lamp} alt="" className="h-10" />
+                            </Circle>
+                        </div>
+                        <div className="border-t-8 border-hijau rounded-md my-5 mx-96 sm-max:my-2 sm-max:mx-10"></div>
+                        <div className="font-bold text-xl h-full text-center sm-max:text-base">
                             <p>Tujuan Bimbingan Konseling ?</p>
                         </div>
-                        <div className="flex justify-center text-gray-400 pt-3 text-lg">
+                        <div className="md:flex md:justify-center text-gray-400 pt-3 sm-max:pt-0 text-lg sm-max:text-sm">
                             <List>
                                 {isiBox}
-                                <div className="flex my-3">
+                                {/* <div className="flex my-3">
                                     <div className="">
                                         <ListIcon as={MdCheckCircle} color='green.500' />
                                     </div>
-                                    <div className="text-center items-center justify-center">Menyesuaikan diri dengan lingkungan pendidikan dan lingkungan masyarakat.</div>
-                                </div>
+                                    {/* <div className="text-center items-center justify-center"></div>
+                                </div> */}
                             </List>
                         </div>
                     </Box>
@@ -246,9 +283,9 @@ export default function Home() {
                     </div>
                     <div className="w-full flex justify-center">
                         <div className="w-3/4 flex justify-around shadow-md text-white p-3 bg-blues rounded-3xl my-14">
-                        <Link smooth={true} duration={1000} to="home"><div className="px-8 rounded-full py-1 hover:bg-hijau">Home</div></Link>
-                        <Link smooth={true} duration={1000} to="bk"><div className="px-8 rounded-full py-1 hover:bg-hijau">Bimbingan Konseling</div></Link>
-                        <Link smooth={true} duration={1000} to="contact"><div className="px-8 rounded-full py-1 hover:bg-hijau">Contact</div></Link>
+                            <Link smooth={true} duration={1000} to="home"><div className="px-8 rounded-full py-1 hover:bg-hijau">Home</div></Link>
+                            <Link smooth={true} duration={1000} to="bk"><div className="px-8 rounded-full py-1 hover:bg-hijau">Bimbingan Konseling</div></Link>
+                            <Link smooth={true} duration={1000} to="contact"><div className="px-8 rounded-full py-1 hover:bg-hijau">Contact</div></Link>
                         </div>
                     </div>
                     <div className="flex justify-center">
