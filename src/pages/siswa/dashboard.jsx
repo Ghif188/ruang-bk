f59aeda7cb792892e874a75ea9fe3b4c80a9f5fe
dzulkifli-mq <santriquarta@gmail.com>
2022-03-04 21:00:15 +0700

    siswa hampir jadi >.<

--- src/pages/siswa/dashboard.jsx
+++ src/pages/siswa/dashboard.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import Jempol from "../../assets/bouken.png"
+import { getProfile } from "../../api/siswa"
+import { useQuery } from "react-query";
 import Layout from "../../layouts/muridlayout"
-import { Table, Th, Td, Thead, Tr, Tbody, Box, Button,Img, Icon } from "@chakra-ui/react";
+import { Table, Th, Td, Thead, Tr, Tbody, Box, Button, Img, Icon } from "@chakra-ui/react";
 export default function Dashboard() {
+    const { isLoading, isError, data, isFetching } = useQuery(
+        [
+            "profile",
+            {
+            },
+        ],
+
+        () =>
+            getProfile({
+            }),
+
+        {
+            keepPreviousData: true,
+            select: (response) => response.data.data,
+        }
+    );
+    console.log(data)
     return (
         <Layout>
             <div className="bg-white bg-opacity-50 h-full w-9/12 px-10 py-2">
@@ -15,32 +34,70 @@ export default function Dashboard() {
                             height='100%'
                         >
                             {/* atas */}
-                            <div className=" rounded-t-2xl bg-gradient-to-r from-sky-500 to-sky-800 py-10 px-20 text-white">
+                            <div className=" rounded-t-2xl bg-gradient-to-r from-sky-500 to-sky-800 py-8 px-20 text-white">
                                 <div className="text-2xl flex">
-                                    <p className="pr-2">Give Your Best</p><img src={Jempol} className="w-9 h-9" alt="" />
+                                    <p className="pr-2">Never Give Up</p><img src={Jempol} className="w-9 h-9" alt="" />
                                 </div>
                                 <div className="w-1/3 py-5">
-                                    <p>The more we are grateful, the more happiness we get.</p>
+                                    <p>Be strong enough to let go and wise enough to wait for what you deserve.</p>
                                 </div>
-                                <div className="">
-                                    <Button
-                                        size='sm'
-                                        height='40px'
-                                        rounded='lg'
-                                        px='25px'
-                                        type='submit'
-                                        variant="solid"
-                                        bgColor='#F0BF12'
-                                        color="white"
-                                    >
-                                        Tambah Angket
-                                    </Button>
+                                <div className="w-full px-5 py-3 flex rounded-lg text-white justify-between bg-oren">
+                                    Pak/Bu {isLoading ? "------" : data.nama_guru}
                                 </div>
                             </div>
                             {/* bawah */}
-                            <div className="font-bold text-xl py-4 px-8">
-                                <div className="">
-                                    <p>iini dashboard</p>
+                            <div className="font-bold text-xl py-4 px-14">
+                                <div className="flex items-center px-5 mb-5  justify-between pb-3">
+                                    <div className="w-8/10 px-5 py-1.5 flex rounded-lg text-white justify-between bg-oren">
+                                        Bahasa Indonesia
+                                    </div>
+                                    <Button
+                                        rounded='lg'
+                                        size='md'
+                                        width='140px'
+                                        colorScheme='twitter'
+                                    >
+                                        Lihat
+                                    </Button>
+                                </div>
+                                <div className="flex items-center px-5 mb-5  justify-between pb-3">
+                                    <div className="w-8/10 px-5 py-1.5 flex rounded-lg text-white justify-between bg-oren">
+                                        Bahasa Indonesia
+                                    </div>
+                                    <Button
+                                        rounded='lg'
+                                        size='md'
+                                        width='140px'
+                                        colorScheme='twitter'
+                                    >
+                                        Lihat
+                                    </Button>
+                                </div>
+                                <div className="flex items-center px-5 mb-5  justify-between pb-3">
+                                    <div className="w-8/10 px-5 py-1.5 flex rounded-lg text-white justify-between bg-oren">
+                                        Bahasa Indonesia
+                                    </div>
+                                    <Button
+                                        rounded='lg'
+                                        size='md'
+                                        width='140px'
+                                        colorScheme='twitter'
+                                    >
+                                        Lihat
+                                    </Button>
+                                </div>
+                                <div className="flex items-center px-5 mb-5  justify-between pb-3">
+                                    <div className="w-8/10 px-5 py-1.5 flex rounded-lg text-white justify-between bg-oren">
+                                        Bahasa Indonesia
+                                    </div>
+                                    <Button
+                                        rounded='lg'
+                                        size='md'
+                                        width='140px'
+                                        colorScheme='twitter'
+                                    >
+                                        Lihat
+                                    </Button>
                                 </div>
                             </div>
                         </Box>
--- src/pages/siswa/profile.jsx
+++ src/pages/siswa/profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../../layouts/muridlayout"
 import { BsPersonCircle } from 'react-icons/bs';
 import { MdEdit } from 'react-icons/md';
-import { getProfile, updateGuru } from '../../api/guru';
+import { getProfile, updateGuru } from '../../api/siswa';
 import { useQuery } from "react-query";
 import { Formik } from "formik";
 import * as Yup from 'yup';
@@ -33,7 +33,7 @@ export default function ProfileSiswa() {
     console.log(data)
     return (
         <Layout>
-            {/* {isLoading ? (
+            {isLoading ? (
                 <div className=" h-full w-10/12 p-8 justify-center flex items-center">
                     <Spinner
                         thickness='5px'
@@ -42,76 +42,82 @@ export default function ProfileSiswa() {
                         color='blue.500'
                         size='xl'
                     />
-                </div>) : */}
-                {/* ( */}
+                </div>) :
+                (
                     <div className="h-full w-10/12 px-20">
-                    <div className="w-full mt-10 rounded-3xl flex h-2/10 shadow-md mb-3 shadow-green-200">
-                        <div className="flex relative rounded-3xl h-full w-full">
-                            <div className="flex w-full h-full rounded-3xl justify-end">
-                                <img src={BgProfile} alt="" className="rounded-3xl absolute h-full w-full" />
-                                <div className="border-r-2 relative text-sky-900 border-sky-600 w-1/3 text-5xl capitalize pr-5 font-bahnschrift font-semibold h-full flex justify-end items-center">
-                                    haloama_guru
-                                </div>
-                                <div className=" w-1/3 h-full pl-5 backdrop-hue-rotate-90 rounded-r-2xl bg-blue-50 bg-opacity-60 backdrop-blur-sm py-10">
-                                    <div className=" font-semibold text-sky-800 text-xl mb-3">halo</div>
-                                    <div className="text-sky-700 text-lg">halo</div>
-                                </div>
-                            </div>
-                            <div className="flex absolute -bottom-1/2 left-5">
-                                    <div className="bg-black p-10 rounded-full">
-                                        <BsPersonCircle className="h-32 w-32 decoration-white text-white" />
+                        <div className="w-full mt-10 rounded-3xl flex h-2/10 shadow-md mb-3 shadow-green-200">
+                            <div className="flex relative rounded-3xl h-full w-full">
+                                <div className="flex w-full h-full rounded-3xl justify-end">
+                                    <img src={BgProfile} alt="" className="rounded-3xl absolute h-full w-full" />
+                                    <div className="border-r-2 relative text-sky-900 border-sky-600 w-1/3 text-5xl capitalize pr-5 font-bahnschrift font-semibold h-full flex justify-end items-center">
+                                        {data.nama_siswa}
                                     </div>
+                                    <div className=" w-1/3 h-full pl-5 backdrop-hue-rotate-90 rounded-r-2xl bg-blue-50 bg-opacity-60 backdrop-blur-sm py-10">
+                                        <div className=" font-semibold text-sky-800 text-xl mb-3">{data.email}</div>
+                                        <div className="text-sky-700 text-lg">{data.nomor_telp}</div>
+                                    </div>
+                                </div>
+                                <div className="flex absolute -bottom-1/2 left-5">
+                                    {data.foto === null ? (
+                                        <div className="bg-black rounded-full shadow-xl">
+                                            <BsPersonCircle className="h-32 w-32 decoration-white text-white" />
+                                        </div>
+                                    ) : (
+                                        <div className="rounded-full shadow-xl">
+                                            <img src={data.foto} className="w-32 h-32 rounded-full" alt="" />
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full p-5 rounded-b-lg h-6/10 shadow-md">
+                            <div className="w-full flex mb-3 justify-center">
+                                <div className="border-b-4 text-2xl font-semibold border-hijau">
+                                    Data Diri
+                                </div>
+                            </div>
+                            <div className="w-full flex justify-between items-center p-5">
+                                <div className="w-1/2 pr-3 h-max text-center rounded-sm">
+                                    <div className="w-full border-b-2 border-sky-700  py-2 flex justify-between">
+                                        <p>NPSN</p>
+                                        <p className="text-lg font-semibold">{data.npsn}</p>
+                                    </div>
+                                    <div className="w-full border-b-2 border-sky-700 mt-6  py-2 flex justify-between">
+                                        <p>Nama Sekolah</p>
+                                        <p className="text-lg font-semibold">{data.sekolah}</p>
+                                    </div>
+                                </div>
+                                <div className="w-1/2 border-l-4 text-center pl-3 rounded-sm">
+                                    <div className="w-full border-b-2 border-sky-700  py-2 flex justify-between">
+                                        <p>Alamat</p>
+                                        <p className="text-lg font-semibold">{data.alamat == null ? "belum diisi" : data.alamat}</p>
+                                    </div>
+                                    <div className="w-full border-b-2 border-sky-700 mt-6  py-2 flex justify-between">
+                                        <p>Tempat Lahir</p>
+                                        <p className="text-lg font-semibold">{data.tempat_lahir == null ? "belum diisi" : data.tempat_lahir}</p>
+                                    </div>
+                                    <div className="w-full border-b-2 border-sky-700 mt-6  py-2 flex justify-between">
+                                        <p>Tanggal lahir</p>
+                                        <p className="text-lg font-semibold">{data.tanggal_lahir == null ? "belum diisi" : data.tanggal_lahir}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="justify-center flex pt-8">
+                                    <Button
+                                        size='lg'
+                                        colorScheme='facebook'
+                                        leftIcon={<MdEdit />}
+                                        onClick={() => navigate("/dash-siswa/edit-profile")}
+                                    >
+                                        Edit
+                                    </Button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full p-5 rounded-b-lg h-6/10 shadow-md">
-                        <div className="w-full flex mb-3 justify-center">
-                            <div className="border-b-4 text-2xl font-semibold border-hijau">
-                                Data Diri
-                            </div>
-                        </div>
-                        <div className="w-full flex justify-between items-center p-5">
-                            <div className="w-1/2 pr-3 h-max text-center rounded-sm">
-                                <div className="w-full border-b-2 border-sky-700  py-2 flex justify-between">
-                                    <p>NPSN</p>
-                                    <p className="text-lg font-semibold">halo</p>
-                                </div>
-                                <div className="w-full border-b-2 border-sky-700 mt-6  py-2 flex justify-between">
-                                    <p>Nama Sekolah</p>
-                                    <p className="text-lg font-semibold">halo</p>
-                                </div>
-                            </div>
-                            <div className="w-1/2 border-l-4 text-center pl-3 rounded-sm">
-                                <div className="w-full border-b-2 border-sky-700  py-2 flex justify-between">
-                                    <p>Alamat</p>
-                                    <p className="text-lg font-semibold">halo</p>
-                                </div>
-                                <div className="w-full border-b-2 border-sky-700 mt-6  py-2 flex justify-between">
-                                    <p>Tempat Lahir</p>
-                                    <p className="text-lg font-semibold">halo</p>
-                                </div>
-                                <div className="w-full border-b-2 border-sky-700 mt-6  py-2 flex justify-between">
-                                    <p>Tanggal lahir</p>
-                                    <p className="text-lg font-semibold">halo</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="justify-center flex pt-8">
-                                <Button
-                                    size='lg'
-                                    colorScheme='facebook'
-                                    leftIcon={<MdEdit />}
-                                    onClick={() => navigate("/dash-guru/edit-profile")}
-                                >
-                                    Edit
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* ) */}
-            {/* } */}
+                )
+            }
         </Layout>
     );
 }
